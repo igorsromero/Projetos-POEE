ec0cfcfaea84c95be0c3964f6da37f009e7f669d
Igor Solfa Romero <igorsolfaromero@gmail.com>
2019-10-10 18:00:28 -0300

    Projeto 2 POEE

---
+++ Projeto 2 POEE/Gadolino2/src/br/com/gadolino2/model/bean/Cadastro.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.gadolino2.model.bean;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+/**
+ *
+ * @author igors
+ */
+@Entity
+public class Cadastro implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Long Id;
+    private String anime;
+    private String genero;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        this.Id = id;
+    }
+
+    public String getAnime() {
+        return anime;
+    }
+
+    public void setAnime(String anime) {
+        this.anime = anime;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }       
+    
+}
